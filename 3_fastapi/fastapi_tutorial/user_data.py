16dcf6bceb15c4240a6280c0d539264717cd66a3
datascienceanywhere <datascienceanywhere@gmail.com>
2026-02-26 11:08:51 +0530

    added data and pdf

---
+++ 3_fastapi/fastapi_tutorial/user_data.py
@@ -0,0 +1,20 @@
+
+class User:
+    def __init__(self):
+        self.user = [
+            {
+                "id":1,
+                "name": "Rahul",
+                "age": 20
+            },
+            {
+                "id":2,
+                "name": "Anita",
+                "age": 21
+            },
+            {
+                "id": 3,
+                "name": "John",
+                "age": 19
+            }
+        ]
